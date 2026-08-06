d05926ef5436f549c61884fb9454d452a4a0740f
Erwin Adelmann <erwin.adelmann@gmail.com>
2026-08-06 22:24:29 +0200

    Videothek: NLP Swish ergänzt

--- src/data/nlp-videos.js
+++ src/data/nlp-videos.js
@@ -18,5 +18,9 @@ export const NLP_VIDEOS = [
   { url: 'https://youtu.be/WIyEJz1OwTU', title: 'Was tun, wenn wir grundlos beleidigt werden, Luftballon-Technik' },
   { url: 'https://youtu.be/CZI2c8NFTaY', title: 'Glaubenssätze 2' },
   { url: 'https://youtu.be/G4-p4La3HfA', title: 'Das Innere Team, Persönlichkeitsanteile im Gespräch' },
-  { url: 'https://youtu.be/U3iaXeAaXL4', title: 'Lösungsorientiertes Fragen, Die Zielkriterien, NLP Coaching Institut Kassel' }
+  { url: 'https://youtu.be/U3iaXeAaXL4', title: 'Lösungsorientiertes Fragen, Die Zielkriterien, NLP Coaching Institut Kassel' },
+  // Liegt zusätzlich in src/data/ressourcen.js (Feature "Ressourcen", Thema NLP). Diese
+  // Dopplung ist von Erwin am 2026-08-05 ausdrücklich gewünscht, abweichend von der sonst
+  // geltenden Regel, bereits in Ressourcen katalogisierte Videos nicht erneut aufzunehmen.
+  { url: 'https://youtu.be/8CQHCU5aMxE', title: 'NLP Swish' }
 ];
